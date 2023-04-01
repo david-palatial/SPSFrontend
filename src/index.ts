42f4e994ba8ae1595a8e0e43ab162b594cbcbf23
David Godfrey <david@palatialxr.com>
2023-04-01 01:37:09 +0000

    SPSFrontend

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
 import { SPSApplication } from "./SPSApplication";
-import { Config, PixelStreaming } from '@epicgames-ps/lib-pixelstreamingfrontend-ue5.2';
+import { Config, PixelStreaming, TextParameters } from '@epicgames-ps/lib-pixelstreamingfrontend-ue5.2';
 import { PixelStreamingApplicationStyle } from '@epicgames-ps/lib-pixelstreamingfrontend-ui-ue5.2';
 
 // Apply default styling from Epic's frontend
 export const PixelStreamingApplicationStyles = new PixelStreamingApplicationStyle();
 PixelStreamingApplicationStyles.applyStyleSheet();
+
+declare var WEBSOCKET_URL: string;
+let signallingServerAddress = WEBSOCKET_URL;
+if (signallingServerAddress == '') {
+    // define our signallingServerProtocol to be used based on whether
+    // or not we're accessing our frontend via a tls
+    let signallingServerProtocol = 'ws:';
+    if (location.protocol === 'https:') {
+        signallingServerProtocol = 'wss:';
+    }
+
+    // build the websocket endpoint based on the protocol used to load the frontend
+    signallingServerAddress = signallingServerProtocol + '//' + window.location.hostname;
+
+    // if the frontend for an application is served from a base-level domain
+    // it has a trailing slash, so we need to account for this when appending the 'ws' for the websocket ingress
+    if (window.location.pathname == "/") {
+        signallingServerAddress += '/ws';
+    } else {
+        signallingServerAddress += (window.location.pathname + '/ws');
+    }
+}
 
 document.body.onload = function () {
 
@@ -14,6 +36,7 @@ document.body.onload = function () {
 	// Create a config object.
 	// Note: This config is extremely important, SPS only supports the browser sending the offer.
 	const config = new Config({ useUrlParams: true, initialSettings: { OfferToReceive: true, TimeoutIfIdle: true } });
+	config.setTextSetting(TextParameters.SignallingServerUrl, signallingServerAddress);
 
 	// Create a Native DOM delegate instance that implements the Delegate interface class
 	const stream = new PixelStreaming(config);
